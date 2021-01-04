24c8e8a08a26aba22909709a37e661d43f3af22b
ismairdrs <ismairdrs@gmail.com>
2021-01-03 21:08:05 -0300

    Crud empresa

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('funcionarios/', include('apps.funcionarios.urls')),
     #path('departamentos/', include('apps.departamentos.urls')),
-    #path('empresas/', include('apps.empresas.urls')),
+    path('empresas/', include('apps.empresas.urls')),
     #path('registro_hora_extra/', include('apps.registro_hora_extra.urls')),
     path('', include('apps.core.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
